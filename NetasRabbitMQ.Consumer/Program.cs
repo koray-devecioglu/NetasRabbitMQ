d63a34823b93865d4a3a413a183c0e24c2f78947
KORAY DEVECİOĞLU <koraydevecioglu@gmail.com>
2020-03-20 02:46:44 +0300

    v1

---
+++ NetasRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace NetasRabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    channel.BasicConsume("hello", true, consumer);
+
+                    consumer.Received += (model, ea) =>
+                      {
+                          var bodyByte = ea.Body;
+                          var message = Encoding.UTF8.GetString(bodyByte);
+
+                          Console.WriteLine("Mesaj alındı: " + message);
+                      };
+
+                    Console.WriteLine("Çıkış yapmak için tıklayınız.");
+                    Console.ReadLine();
+                }
+
+
+            }
+        }
+    }
+}
---
+++ NetasRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace NetasRabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    string message = "Hello Netas";
+
+                    var bodyByte = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", routingKey: "hello", null, body:bodyByte);
+
+                    Console.WriteLine("Mesajınız gönderilmiştir.");
+
+                }
+
+                Console.WriteLine("Çıkış yapmak için tıklayınız.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
